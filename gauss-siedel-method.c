0569a42f655006cabaf5ab6099bc5c7cf3c2e308
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2023-11-28 14:39:26 +0000

    Fix inputs, WIP

--- gauss-siedel-method.c
+++ gauss-siedel-method.c
@@ -3,24 +3,33 @@
 
 int main (void)
 {
-    int coef[N_EQ][N_EQ + 1], i, j;
+    double eqn[N_EQ][N_EQ + 1];
+    int i = 0, j = 0;
 
     printf("*** Gauss Seidel Method ***\n");
     printf("The general format is ax + by + cz = d\n");
     for (i = 0; i < N_EQ; i++)
     {
         printf("Equation %d\n", i+1);
-        printf("Enter the values of a, b, c, d (space-seperated): ");
-        scanf("%lf %lf %lf %lf", &coef[0], &coef[1], &coef[2], &coef[3]);
+        readEquation(eqn[i]);
+
     }
     printf("The equations are:\n");
     for (i = 0; i < N_EQ; i++)
     {
-        for (j = 0; j < N_EQ + 1; j++)
-        {
-            printf("%lf ", coef[i][j]);
-        }
-        printf("\n");
+        printEquation(eqn[i]);
     }
     return 0;
+}
+
+void readEquation (int coeff[N_EQ + 1])
+{
+    printf("Enter the values of a, b, c, d (space-seperated): ");
+    scanf("%lf %lf %lf %lf", &coeff[0], &coeff[1], &coeff[2], &coeff[3]);
+}
+
+void printEquation (int coeff[N_EQ + 1])
+{
+    // print the equation with the given coefficients, in the format ax + by + cz = d
+    printf("(%.2lf)x + (%.2lf)y + (%.2lf)z = (%.2lf)\n", coeff[0], coeff[1], coeff[2], coeff[3]);
 }
