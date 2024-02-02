c3040fecdf7574f35996980160a7f464309ce984
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-02-02 23:28:44 +0530

    Spacing updated

--- Differential-Eqn-Solving/euler-method.c
+++ Differential-Eqn-Solving/euler-method.c
@@ -11,11 +11,11 @@ int main ()
     double x[MAX_SIZE], y[MAX_SIZE], h, a;
     int i;
 
-    printf("Enter x[0], y[0]:");
+    printf("Enter x[0], y[0]: ");
     scanf("%lf %lf", &x[0], &y[0]);
-    printf("Enter h (step length):");
+    printf("Enter h (step length): ");
     scanf("%lf", &h);
-    printf("Enter a (the value to calculate):");
+    printf("Enter a (the value to calculate): ");
     scanf("%lf", &a);
 
     i = 0;
