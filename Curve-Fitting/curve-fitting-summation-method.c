218ba19555db8b883e77f19acaed60610ab01a3c
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-02-04 16:24:15 +0530

    Indented

--- Curve-Fitting/curve-fitting-summation-method.c
+++ Curve-Fitting/curve-fitting-summation-method.c
@@ -1,33 +1,31 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
+
 int main()
 {
-
-    int n,i,x[20],y[20],sumx=0,sumy=0,sumxy=0,sumx2=0;
-    float a,b;
+    int n, i, x[20], y[20], sumx = 0, sumy = 0, sumxy = 0, sumx2 = 0;
+    float a, b;
     printf("\n Enter the value of number of terms n:");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("\n Enter the values of x:\n");
-    for(i=0;i<=n-1;i++)
+    for (i = 0; i <= n - 1; i++)
     {
-        scanf(" %d",&x[i]);
-
+        scanf(" %d", &x[i]);
     }
     printf("\n Enter the values of y:");
-    for(i=0;i<=n-1;i++)
+    for (i = 0; i <= n - 1; i++)
     {
-        scanf("%d",&y[i]);
+        scanf("%d", &y[i]);
     }
-    for(i=0;i<=n-1;i++)
+    for (i = 0; i <= n - 1; i++)
     {
-        sumx=sumx +x[i];
-        sumx2=sumx2 +x[i]*x[i];
-        sumy=sumy +y[i];
-        sumxy=sumxy +x[i]*y[i];
-
+        sumx = sumx + x[i];
+        sumx2 = sumx2 + x[i] * x[i];
+        sumy = sumy + y[i];
+        sumxy = sumxy + x[i] * y[i];
     }
-    a=((sumx2*sumy -sumx*sumxy)*1.0/(n*sumx2-sumx*sumx)*1.0);
-    b=((n*sumxy-sumx*sumy)*1.0/(n*sumx2-sumx*sumx)*1.0);
-    printf("\n\nThe line is Y=%3.3f +%3.3f X",a,b);
-    return(0);
+    a = ((sumx2 * sumy - sumx * sumxy) * 1.0 / (n * sumx2 - sumx * sumx) * 1.0);
+    b = ((n * sumxy - sumx * sumy) * 1.0 / (n * sumx2 - sumx * sumx) * 1.0);
+    printf("\n\nThe line is Y=%3.3f +%3.3f X", a, b);
+    return (0);
 }
